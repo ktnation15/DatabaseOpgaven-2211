e3a0f74617e43f4a656f482eeaa8aa738639e07d
Klaus Eigil Erik Avgo Thor Therkildsen <ktnation15@gmail.com>
2023-11-22 22:40:04 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace DatabaseOpgaven_2211
+{
+    partial class FacilitiesFacilityIDLbl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FacilityFacilityIdLbl = new System.Windows.Forms.Label();
+            this.FacilitiesHotelNoLbl = new System.Windows.Forms.Label();
+            this.FacilitiesFacilityNameLbl = new System.Windows.Forms.Label();
+            this.FacilitiesDescriptionLbl = new System.Windows.Forms.Label();
+            this.txtFacilityId = new System.Windows.Forms.TextBox();
+            this.txtHotelNo = new System.Windows.Forms.TextBox();
+            this.txtFacilityName = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.InsertFacilitiesBtn = new System.Windows.Forms.Button();
+            this.UpdateFacilitiesBtn = new System.Windows.Forms.Button();
+            this.DeleteFacilitiesBtn = new System.Windows.Forms.Button();
+            this.ShowAllFacilitiesBtn = new System.Windows.Forms.Button();
+            this.ExitFacilitiesBtn = new System.Windows.Forms.Button();
+            this.MenuFacilitiesBtn = new System.Windows.Forms.Button();
+            this.FindBarFacilities = new System.Windows.Forms.TextBox();
+            this.FindFacilitiesBtn = new System.Windows.Forms.Button();
+            this.dataGridViewFacilities = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFacilities)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FacilityFacilityIdLbl
+            // 
+            this.FacilityFacilityIdLbl.AutoSize = true;
+            this.FacilityFacilityIdLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FacilityFacilityIdLbl.Location = new System.Drawing.Point(22, 42);
+            this.FacilityFacilityIdLbl.Name = "FacilityFacilityIdLbl";
+            this.FacilityFacilityIdLbl.Size = new System.Drawing.Size(128, 29);
+            this.FacilityFacilityIdLbl.TabIndex = 9;
+            this.FacilityFacilityIdLbl.Text = "Facility ID";
+            // 
+            // FacilitiesHotelNoLbl
+            // 
+            this.FacilitiesHotelNoLbl.AutoSize = true;
+            this.FacilitiesHotelNoLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FacilitiesHotelNoLbl.Location = new System.Drawing.Point(22, 83);
+            this.FacilitiesHotelNoLbl.Name = "FacilitiesHotelNoLbl";
+            this.FacilitiesHotelNoLbl.Size = new System.Drawing.Size(116, 29);
+            this.FacilitiesHotelNoLbl.TabIndex = 11;
+            this.FacilitiesHotelNoLbl.Text = "Hotel No";
+            // 
+            // FacilitiesFacilityNameLbl
+            // 
+            this.FacilitiesFacilityNameLbl.AutoSize = true;
+            this.FacilitiesFacilityNameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FacilitiesFacilityNameLbl.Location = new System.Drawing.Point(22, 123);
+            this.FacilitiesFacilityNameLbl.Name = "FacilitiesFacilityNameLbl";
+            this.FacilitiesFacilityNameLbl.Size = new System.Drawing.Size(172, 29);
+            this.FacilitiesFacilityNameLbl.TabIndex = 12;
+            this.FacilitiesFacilityNameLbl.Text = "Facility Name";
+            // 
+            // FacilitiesDescriptionLbl
+            // 
+            this.FacilitiesDescriptionLbl.AutoSize = true;
+            this.FacilitiesDescriptionLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FacilitiesDescriptionLbl.Location = new System.Drawing.Point(22, 163);
+            this.FacilitiesDescriptionLbl.Name = "FacilitiesDescriptionLbl";
+            this.FacilitiesDescriptionLbl.Size = new System.Drawing.Size(146, 29);
+            this.FacilitiesDescriptionLbl.TabIndex = 13;
+            this.FacilitiesDescriptionLbl.Text = "Description";
+            // 
+            // txtFacilityId
+            // 
+            this.txtFacilityId.Location = new System.Drawing.Point(194, 42);
+            this.txtFacilityId.Name = "txtFacilityId";
+            this.txtFacilityId.Size = new System.Drawing.Size(198, 26);
+            this.txtFacilityId.TabIndex = 14;
+            // 
+            // txtHotelNo
+            // 
+            this.txtHotelNo.Location = new System.Drawing.Point(194, 83);
+            this.txtHotelNo.Name = "txtHotelNo";
+            this.txtHotelNo.Size = new System.Drawing.Size(198, 26);
+            this.txtHotelNo.TabIndex = 15;
+            // 
+            // txtFacilityName
+            // 
+            this.txtFacilityName.Location = new System.Drawing.Point(194, 123);
+            this.txtFacilityName.Name = "txtFacilityName";
+            this.txtFacilityName.Size = new System.Drawing.Size(198, 26);
+            this.txtFacilityName.TabIndex = 16;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(194, 167);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(198, 26);
+            this.txtDescription.TabIndex = 17;
+            // 
+            // InsertFacilitiesBtn
+            // 
+            this.InsertFacilitiesBtn.Location = new System.Drawing.Point(23, 218);
+            this.InsertFacilitiesBtn.Name = "InsertFacilitiesBtn";
+            this.InsertFacilitiesBtn.Size = new System.Drawing.Size(119, 39);
+            this.InsertFacilitiesBtn.TabIndex = 23;
+            this.InsertFacilitiesBtn.Text = "Insert";
+            this.InsertFacilitiesBtn.UseVisualStyleBackColor = true;
+            this.InsertFacilitiesBtn.Click += new System.EventHandler(this.InsertFacilitiesBtn_Click);
+            // 
+            // UpdateFacilitiesBtn
+            // 
+            this.UpdateFacilitiesBtn.Location = new System.Drawing.Point(148, 218);
+            this.UpdateFacilitiesBtn.Name = "UpdateFacilitiesBtn";
+            this.UpdateFacilitiesBtn.Size = new System.Drawing.Size(119, 39);
+            this.UpdateFacilitiesBtn.TabIndex = 24;
+            this.UpdateFacilitiesBtn.Text = "Update";
+            this.UpdateFacilitiesBtn.UseVisualStyleBackColor = true;
+            this.UpdateFacilitiesBtn.Click += new System.EventHandler(this.UpdateFacilitiesBtn_Click);
+            // 
+            // DeleteFacilitiesBtn
+            // 
+            this.DeleteFacilitiesBtn.Location = new System.Drawing.Point(273, 218);
+            this.DeleteFacilitiesBtn.Name = "DeleteFacilitiesBtn";
+            this.DeleteFacilitiesBtn.Size = new System.Drawing.Size(119, 39);
+            this.DeleteFacilitiesBtn.TabIndex = 25;
+            this.DeleteFacilitiesBtn.Text = "Delete";
+            this.DeleteFacilitiesBtn.UseVisualStyleBackColor = true;
+            this.DeleteFacilitiesBtn.Click += new System.EventHandler(this.DeleteFacilitiesBtn_Click);
+            // 
+            // ShowAllFacilitiesBtn
+            // 
+            this.ShowAllFacilitiesBtn.Location = new System.Drawing.Point(23, 272);
+            this.ShowAllFacilitiesBtn.Name = "ShowAllFacilitiesBtn";
+            this.ShowAllFacilitiesBtn.Size = new System.Drawing.Size(119, 39);
+            this.ShowAllFacilitiesBtn.TabIndex = 26;
+            this.ShowAllFacilitiesBtn.Text = "Show All";
+            this.ShowAllFacilitiesBtn.UseVisualStyleBackColor = true;
+            this.ShowAllFacilitiesBtn.Click += new System.EventHandler(this.ShowAllFacilitiesBtn_Click);
+            // 
+            // ExitFacilitiesBtn
+            // 
+            this.ExitFacilitiesBtn.Location = new System.Drawing.Point(148, 272);
+            this.ExitFacilitiesBtn.Name = "ExitFacilitiesBtn";
+            this.ExitFacilitiesBtn.Size = new System.Drawing.Size(119, 39);
+            this.ExitFacilitiesBtn.TabIndex = 27;
+            this.ExitFacilitiesBtn.Text = "Exit";
+            this.ExitFacilitiesBtn.UseVisualStyleBackColor = true;
+            this.ExitFacilitiesBtn.Click += new System.EventHandler(this.ExitFacilitiesBtn_Click);
+            // 
+            // MenuFacilitiesBtn
+            // 
+            this.MenuFacilitiesBtn.Location = new System.Drawing.Point(273, 272);
+            this.MenuFacilitiesBtn.Name = "MenuFacilitiesBtn";
+            this.MenuFacilitiesBtn.Size = new System.Drawing.Size(119, 39);
+            this.MenuFacilitiesBtn.TabIndex = 28;
+            this.MenuFacilitiesBtn.Text = "Menu";
+            this.MenuFacilitiesBtn.UseVisualStyleBackColor = true;
+            this.MenuFacilitiesBtn.Click += new System.EventHandler(this.MenuFacilitiesBtn_Click);
+            // 
+            // FindBarFacilities
+            // 
+            this.FindBarFacilities.Location = new System.Drawing.Point(413, 42);
+            this.FindBarFacilities.Name = "FindBarFacilities";
+            this.FindBarFacilities.Size = new System.Drawing.Size(910, 26);
+            this.FindBarFacilities.TabIndex = 29;
+            // 
+            // FindFacilitiesBtn
+            // 
+            this.FindFacilitiesBtn.Location = new System.Drawing.Point(1340, 29);
+            this.FindFacilitiesBtn.Name = "FindFacilitiesBtn";
+            this.FindFacilitiesBtn.Size = new System.Drawing.Size(119, 39);
+            this.FindFacilitiesBtn.TabIndex = 30;
+            this.FindFacilitiesBtn.Text = "Find";
+            this.FindFacilitiesBtn.UseVisualStyleBackColor = true;
+            this.FindFacilitiesBtn.Click += new System.EventHandler(this.FindFacilitiesBtn_Click);
+            // 
+            // dataGridViewFacilities
+            // 
+            this.dataGridViewFacilities.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewFacilities.Location = new System.Drawing.Point(413, 83);
+            this.dataGridViewFacilities.Name = "dataGridViewFacilities";
+            this.dataGridViewFacilities.RowHeadersWidth = 62;
+            this.dataGridViewFacilities.RowTemplate.Height = 28;
+            this.dataGridViewFacilities.Size = new System.Drawing.Size(1046, 399);
+            this.dataGridViewFacilities.TabIndex = 31;
+            // 
+            // FacilitiesFacilityIDLbl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1607, 670);
+            this.Controls.Add(this.dataGridViewFacilities);
+            this.Controls.Add(this.FindFacilitiesBtn);
+            this.Controls.Add(this.FindBarFacilities);
+            this.Controls.Add(this.MenuFacilitiesBtn);
+            this.Controls.Add(this.ExitFacilitiesBtn);
+            this.Controls.Add(this.ShowAllFacilitiesBtn);
+            this.Controls.Add(this.DeleteFacilitiesBtn);
+            this.Controls.Add(this.UpdateFacilitiesBtn);
+            this.Controls.Add(this.InsertFacilitiesBtn);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtFacilityName);
+            this.Controls.Add(this.txtHotelNo);
+            this.Controls.Add(this.txtFacilityId);
+            this.Controls.Add(this.FacilitiesDescriptionLbl);
+            this.Controls.Add(this.FacilitiesFacilityNameLbl);
+            this.Controls.Add(this.FacilitiesHotelNoLbl);
+            this.Controls.Add(this.FacilityFacilityIdLbl);
+            this.Name = "FacilitiesFacilityIDLbl";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FacilitiesFacilityIDLbl_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFacilities)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label FacilityFacilityIdLbl;
+        private System.Windows.Forms.Label FacilitiesHotelNoLbl;
+        private System.Windows.Forms.Label FacilitiesFacilityNameLbl;
+        private System.Windows.Forms.Label FacilitiesDescriptionLbl;
+        private System.Windows.Forms.TextBox txtFacilityId;
+        private System.Windows.Forms.TextBox txtHotelNo;
+        private System.Windows.Forms.TextBox txtFacilityName;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Button InsertFacilitiesBtn;
+        private System.Windows.Forms.Button UpdateFacilitiesBtn;
+        private System.Windows.Forms.Button DeleteFacilitiesBtn;
+        private System.Windows.Forms.Button ShowAllFacilitiesBtn;
+        private System.Windows.Forms.Button ExitFacilitiesBtn;
+        private System.Windows.Forms.Button MenuFacilitiesBtn;
+        private System.Windows.Forms.TextBox FindBarFacilities;
+        private System.Windows.Forms.Button FindFacilitiesBtn;
+        private System.Windows.Forms.DataGridView dataGridViewFacilities;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseOpgaven_2211
+{
+    public partial class FacilitiesFacilityIDLbl : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        public FacilitiesFacilityIDLbl()
+        {
+            InitializeComponent();
+        }
+
+        private void FacilitiesFacilityIDLbl_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=HotelDatabase;Integrated Security=True");
+            cmd = new SqlCommand();
+            cmd.Connection = conn;
+        }
+
+        private void InsertFacilitiesBtn_Click(object sender, EventArgs e)
+        {
+            string query = $"insert into Facilities values('{txtFacilityId.Text.ToString()}','{txtHotelNo.Text.ToString()}','{txtFacilityName.Text}','{txtDescription.Text}')";
+            cmd.CommandText = query;
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            cleardata();
+            conn.Close();
+            displaydata();
+        }
+        private void cleardata()
+        {
+            txtFacilityId.Clear();
+            txtHotelNo.Clear();
+            txtFacilityName.Clear();
+            txtDescription.Clear();
+        }
+
+        private void UpdateFacilitiesBtn_Click(object sender, EventArgs e)
+        {
+            //conn.Open();
+            //SqlCommand cmd = conn.CreateCommand();
+            //cmd.CommandType = CommandType.Text;
+            //cmd.CommandText = "UPDATE Facilities set FacilityID='" + txtFacilityId.Text.ToString() + "',Hotel_no='" + txtHotelNo.Text.ToString() + "',FacilityName='" + txtFacilityName.Text + "' where Description='" + txtDescription.Text + "' ";
+            //cmd.ExecuteNonQuery();
+            //conn.Close();
+            //displaydata();
+            //cleardata();
+
+            conn.Open();
+            cmd.Parameters.Clear();
+
+            StringBuilder updateQuery = new StringBuilder("UPDATE Facilities SET ");
+
+            if (!string.IsNullOrEmpty(txtFacilityId.Text))
+            {
+                updateQuery.Append("FacilityID = @FacilityID, ");
+                cmd.Parameters.AddWithValue("@FacilityID", txtFacilityId.Text.ToString());
+            }
+
+            if (!string.IsNullOrEmpty(txtHotelNo.Text))
+            {
+                updateQuery.Append("Hotel_no = @Hotel_no, ");
+                cmd.Parameters.AddWithValue("@Hotel_no", txtHotelNo.Text.ToString());
+            }
+
+            if (!string.IsNullOrEmpty(txtFacilityName.Text))
+            {
+                updateQuery.Append("FacilityName = @FacilityName, ");
+                cmd.Parameters.AddWithValue("@FacilityName", txtFacilityName.Text);
+            }
+
+            if (!string.IsNullOrEmpty(txtDescription.Text))
+            {
+                updateQuery.Append("Description = @Description, ");
+                cmd.Parameters.AddWithValue("@Description", txtDescription.Text);
+            }
+
+            updateQuery.Length -= 2;
+            updateQuery.Append(" WHERE FacilityID = @FacilityID AND Hotel_no = @Hotel_no AND FacilityName = @FacilityName AND Description = @Description");
+
+            cmd.CommandText = updateQuery.ToString();
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            displaydata();
+            cleardata();
+        }
+
+        private void ShowAllFacilitiesBtn_Click(object sender, EventArgs e)
+        {
+            displaydata();
+        }
+        private void displaydata()
+        {
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Facilities";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridViewFacilities.DataSource = dt;
+            conn.Close();
+        }
+
+        private void DeleteFacilitiesBtn_Click(object sender, EventArgs e)
+        {
+            string query = $"delete Facilities where FacilityID='{txtFacilityId.Text.ToString()}'";
+            cmd.CommandText = query;
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            dataGridViewFacilities.DataSource = query;
+            cleardata();
+            conn.Close();
+            displaydata();
+        }
+
+        private void FindFacilitiesBtn_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Facilities where FacilityID='" + FindBarFacilities.Text + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            txtHotelNo.Text = dt.ToString();
+            txtFacilityName.Text = dt.ToString();
+            txtDescription.Text = dt.ToString();
+            dataGridViewFacilities.DataSource = dt;
+            conn.Close();
+        }
+
+        private void ExitFacilitiesBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MenuFacilitiesBtn_Click(object sender, EventArgs e)
+        {
+            new MainMenu();
+        }
+    }
+}
